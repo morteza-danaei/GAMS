d738a1b97118be9a73788dc37e877e320ca2e640
morteza.danaei <mortez.danaei@gmail.com>
2023-10-07 14:07:55 +0330

    refactor(auth-service): move currentUser middleware to a helper module

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,37 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 import { BadRequestError } from "../errorHandler/errors/bad-request-error";
-
-interface UserPayload {
-  id: string;
-  email: string;
-}
-
-declare global {
-  namespace Express {
-    interface Request {
-      currentUser?: UserPayload;
-    }
-  }
-}
-
-// a middleware to extract the current user
-// from jwt token
-const currentUser = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.session?.jwt) {
-    return next();
-  }
-
-  try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
-    ) as UserPayload;
-    req.currentUser = payload;
-  } catch (err) {}
-
-  next();
-};
+import { currentUser } from "../helpers/current-user";
 
 const router = express.Router();
 
