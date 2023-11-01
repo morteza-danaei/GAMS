ffbc9b32b3c3c96a0b1e4e792f64485099f0e4db
morteza.danaei <mortez.danaei@gmail.com>
2023-11-01 08:00:24 +0330

    refactor(personnel-service): move commonused modules to utility  npm package
    move publisher class, events interface, and subjects from personnel service to  @gasm/utility

--- personnel/src/helpers/event/personnel-created-publisher.ts
+++ personnel/src/helpers/event/personnel-created-publisher.ts
@@ -1,45 +1,4 @@
-import { Stan } from "node-nats-streaming";
-
-enum Subjects {
-  PersonnelCreated = "Personnel:created",
-}
-
-interface Event {
-  subject: Subjects;
-  data: any;
-}
-
-interface PersonnelCreatedEvent {
-  subject: Subjects.PersonnelCreated;
-  data: {
-    nid: string;
-    pid: string;
-    name: string;
-    lastname: string;
-    department: string;
-  };
-}
-
-abstract class Publisher<T extends Event> {
-  abstract subject: T["subject"];
-  protected client: Stan;
-
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  publish(data: T["data"]): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.client.publish(this.subject, JSON.stringify(data), (err) => {
-        if (err) {
-          return reject(err);
-        }
-        console.log("Event published to subject", this.subject);
-        resolve();
-      });
-    });
-  }
-}
+import { Subjects, Publisher, PersonnelCreatedEvent } from "@gams/utility";
 
 export class PersonnelCreatedPublisher extends Publisher<PersonnelCreatedEvent> {
   subject: Subjects.PersonnelCreated = Subjects.PersonnelCreated;
--- personnel/src/helpers/nats-connector.ts
+++ personnel/src/helpers/nats-connector.ts
@@ -1,5 +1,13 @@
 import nats, { Stan } from "node-nats-streaming";
 
+/**
+
+ * A class that provides a connection to a NATS server.
+
+ * This class can be used to connect to a NATS server and publish and subscribe to messages. 
+ * It also provides a convenient way to manage the NATS connection, such as automatically reconnecting if the connection is lost.
+ * @remarks This class is thread-safe and can be used in multiple threads simultaneously. 
+ */
 class NatsConnector {
   private _client?: Stan;
 
@@ -11,6 +19,15 @@ class NatsConnector {
     return this._client;
   }
 
+  /**
+
+   * Connects to the NATS server.
+
+   * @param clusterId The NATS cluster ID.
+   * @param clientId The NATS client ID.
+   * @param url The NATS server URL.
+   * @returns A promise that resolves when the connection is established. 
+   */
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
 
