e0bc9b66b1b22e2f28431a8d6e31ada568804fd1
morteza.danaei <mortez.danaei@gmail.com>
2023-11-04 08:58:51 +0330

    test(personnel-service): add show  test file to show route

---
+++ personnel/src/routes/__test__/show.test.ts
@@ -0,0 +1,79 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+
+import { validPersonnel } from "../../helpers/test.helper";
+import { Personnel } from "../../models/personnel.model";
+
+let prsnlId: string;
+
+/**
+ * Creates a new  Personnel  document in db and fetches the ID of a personnel.
+
+ * This function makes a POST request to the `/api/personnels` endpoint to create a new personnel. 
+ * It then fetches all personnels from the database and returns the ID of the first and only personnel since the db was empty before.
+
+ * It assigns the prsnlId variable with the ID of a personnel created by the current user.
+ */
+const personnelId = async () => {
+  const response = await request(app)
+    .post("/api/personnels")
+    .set("Cookie", await global.signin())
+    .send(validPersonnel);
+  const personnels = await Personnel.find(validPersonnel);
+  expect(personnels.length).toBe(1);
+  prsnlId = personnels[0].id.toString();
+};
+
+it("has a route handler listening to /api/personnels/:id for get requests", async () => {
+  await personnelId();
+
+  const response = await request(app)
+    .get(`/api/personnels/${prsnlId}`)
+    .set("Cookie", await global.signin());
+  expect(response.status).not.toBe(400);
+});
+
+it("can only be accessed if the user is signed in", async () => {
+  await personnelId();
+
+  await request(app).get(`/api/personnels/${prsnlId}`).expect(401);
+});
+
+it("returns 401 when cookie is tampered/invalid", async () => {
+  await personnelId();
+  await request(app)
+    .get(`/api/personnels/${prsnlId}`)
+    .set("Cookie", "fsadfsdfsadfsa")
+    .expect(401);
+});
+
+it("returns a status other than 401 if the user is signed in", async () => {
+  await personnelId();
+  const response = await request(app)
+    .get(`/api/personnels/${prsnlId}`)
+    .set("Cookie", await global.signin());
+
+  expect(response.status).not.toEqual(401);
+});
+
+it("returns a 404 if the personnel is not found", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
+});
+
+it("returns the personnel if the personnel is found", async () => {
+  await personnelId();
+
+  const response = await request(app)
+    .get(`/api/personnels/${prsnlId}`)
+    .set("Cookie", await global.signin())
+    .expect(200);
+
+  expect(response.body.pid).toEqual("fjksdhdd");
+  expect(response.body.nid).toEqual("ksdjflskdj");
+  expect(response.body.name).toEqual("kdjsla");
+  expect(response.body.lastname).toEqual("sadhf");
+  expect(response.body.department).toEqual("fsadf");
+});
--- personnel/src/routes/show.route.ts
+++ personnel/src/routes/show.route.ts
@@ -5,6 +5,13 @@ import { Personnel } from "../models/personnel.model";
 const router = express.Router();
 
 router.get("/api/personnels/:id", async (req: Request, res: Response) => {
+  /**
+   * Checks if the user is authenticated. If the user is not authenticated, the router returns a 401 Unauthorized response.
+   */
+  if (!req.currentUser) {
+    return res.status(401).send({ message: "Not Authorized" });
+  }
+
   const personnel = await Personnel.findById(req.params.id);
 
   if (!personnel) {
